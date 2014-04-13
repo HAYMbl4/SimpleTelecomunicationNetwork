36bc64743f3c51ca5897758dfe2d21ce51bfcf43
Oleg Naumov <naumov.olega@gmail.com>
2014-04-13 22:46:02 +0400

    Интерфейс для общения с БД типов узлов

---
+++ src/main/java/dao/interfaces/NodeTypeDAO.java
@@ -0,0 +1,18 @@
+package dao.interfaces;
+
+import entity.NodeType;
+
+import java.util.List;
+
+/**
+ *
+ * User: o.naumov
+ * Date: 13.04.14 Time: 17:01
+ *
+ */
+
+public interface NodeTypeDAO {
+
+    List<NodeType> getListNodeType();
+
+}
