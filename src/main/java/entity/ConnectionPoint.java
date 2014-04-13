465b9520a57c991a9cc59e3c77816fa22b413dcd
Oleg Naumov <naumov.olega@gmail.com>
2014-04-13 22:45:09 +0400

    Контейнер для перечня точек ОКУ

---
+++ src/main/java/entity/ConnectionPoint.java
@@ -0,0 +1,69 @@
+package entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ *
+ * User: o.naumov
+ * Date: 13.04.14 Time: 01:18
+ *
+ */
+
+@Entity
+@Table(name = "connection_point")
+public class ConnectionPoint implements Serializable {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "GEN_CP")
+    @SequenceGenerator(name = "GEN_CP", sequenceName = "GEN_CP", allocationSize = 1)
+    @Column(name = "cp_id")
+    private Long cpId;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY, targetEntity = ConnectionUnit.class)
+    @JoinColumn(name = "cu_id")
+    private ConnectionUnit connectionUnit;
+
+    @Column(name = "cp_name", nullable = false)
+    private Long cpName;
+
+    public ConnectionPoint() {
+    }
+
+    public ConnectionPoint(Long cpName, ConnectionUnit connectionUnit) {
+        this.cpName = cpName;
+        this.connectionUnit = connectionUnit;
+    }
+
+    public Long getCpId() {
+        return cpId;
+    }
+
+    public void setCpId(Long cpId) {
+        this.cpId = cpId;
+    }
+
+    public ConnectionUnit getConnectionUnit() {
+        return connectionUnit;
+    }
+
+    public void setConnectionUnit(ConnectionUnit connectionUnit) {
+        this.connectionUnit = connectionUnit;
+    }
+
+    public Long getCpName() {
+        return cpName;
+    }
+
+    public void setCpName(Long cpName) {
+        this.cpName = cpName;
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectionPoint{ cp_id = " + cpId + "," +
+                "                cu_id = " + connectionUnit.getCuId() + "(" + connectionUnit.getNode().getNodeType().getNodeTypeShortName() +
+                                            "" + connectionUnit.getNode().getNodeName() + "-" + connectionUnit.getCuNumber() + ")," +
+                "                cp_name = " + cpName;
+    }
+}
