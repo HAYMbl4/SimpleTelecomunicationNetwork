96b8aec09831e5bfa40c8eda1e66bcb6de3871b0
Oleg Naumov <naumov.olega@gmail.com>
2014-04-13 22:47:51 +0400

    Реализация методов общения с БД - для узлов

---
+++ src/main/java/dao/service/ServiceNode.java
@@ -0,0 +1,44 @@
+package dao.service;
+
+import dao.interfaces.NodeDAO;
+import entity.Node;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+/**
+ *
+ * User: o.naumov
+ * Date: 12.04.14 Time: 23:52
+ *
+ */
+
+public class ServiceNode implements NodeDAO {
+
+    public List<Node> getListNode() {
+
+        ServiceNode sn = new ServiceNode();
+        Session session = sn.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Query query = session.createQuery("from Node");
+        List<Node> nodeList = query.list();
+        System.out.println("erge");
+        for (Node n: nodeList) {
+            System.out.println(n.toString());
+        }
+
+        session.flush();
+        session.close();
+
+        return nodeList;
+    }
+
+    protected SessionFactory getSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+}
