59e595b6dd75c01592f19f374d36099a2d24da47
Oleg Naumov <naumov.olega@gmail.com>
2014-04-13 22:46:44 +0400

    Интерфейс для общения с БД - точек ОКУ

---
+++ src/main/java/dao/interfaces/ConnectionPointDAO.java
@@ -0,0 +1,16 @@
+package dao.interfaces;
+
+import entity.ConnectionPoint;
+
+import java.util.List;
+
+/**
+ * User: Олег Наумов
+ * Date: 4/13/14 Time: 10:16 PM
+ */
+
+public interface ConnectionPointDAO {
+
+    List<ConnectionPoint> getListConnectionPoint();
+
+}
