81576899457a8f799d01440c644d07d84634f54d
Oleg Naumov <naumov.olega@gmail.com>
2014-04-13 22:46:33 +0400

    Интерфейс для общения с БД - ОКУ

---
+++ src/main/java/dao/interfaces/ConnectionUnitDAO.java
@@ -0,0 +1,16 @@
+package dao.interfaces;
+
+import entity.ConnectionUnit;
+
+import java.util.List;
+
+/**
+ * User: Олег Наумов
+ * Date: 4/13/14 Time: 10:06 PM
+ */
+
+public interface ConnectionUnitDAO {
+
+    List<ConnectionUnit> getListConnectionUnit();
+
+}
