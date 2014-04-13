77d2ed4b9bc4e716226668712ce3fadbf01724da
Oleg Naumov <naumov.olega@gmail.com>
2014-04-13 22:46:23 +0400

    Интерфейс для общения с БД - узлов

---
+++ src/main/java/dao/interfaces/NodeDAO.java
@@ -0,0 +1,18 @@
+package dao.interfaces;
+
+import entity.Node;
+
+import java.util.List;
+
+/**
+ *
+ * User: o.naumov
+ * Date: 13.04.14 Time: 21:43
+ *
+ */
+
+public interface NodeDAO {
+
+    List<Node> getListNode();
+
+}
