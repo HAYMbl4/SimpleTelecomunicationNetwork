3cbb8b0a2c4947d90af3285215087ff1472e4353
Oleg Naumov <naumov.olega@gmail.com>
2014-04-15 09:58:15 +0400

    В bean ОКУ добавил метод получения списка ОКУ по ID узла

--- src/main/java/bean/ConnectionUnitBean.java
+++ src/main/java/bean/ConnectionUnitBean.java
@@ -3,7 +3,10 @@ package bean;
 import entity.ConnectionUnit;
 import dao.service.ServiceConnectionUnit;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import java.util.List;
 
 /**
@@ -12,11 +15,35 @@ import java.util.List;
  */
 
 @ManagedBean(name = "cuBean")
+@RequestScoped
 public class ConnectionUnitBean {
+
+    @ManagedProperty("#{param.nodeid}")
+    private Long nodeId;
 
     public List<ConnectionUnit> getListConnectionUnit() {
         ServiceConnectionUnit serviceConnectionUnit = new ServiceConnectionUnit();
         return serviceConnectionUnit.getListConnectionUnit();
     }
 
+    @PostConstruct
+    public List<ConnectionUnit> getConnectionUnitByNode() {
+
+        List<ConnectionUnit> listCU = null;
+
+        ServiceConnectionUnit serviceConnectionUnit = new ServiceConnectionUnit();
+        if (nodeId != null) {
+           listCU = serviceConnectionUnit.getConnectionUnitByNode(nodeId);
+        }
+
+        return listCU;
+    }
+
+    public Long getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(Long nodeId) {
+        this.nodeId = nodeId;
+    }
 }
