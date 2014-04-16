7cd1cb2f5d3f3516a398e36624d3af97ecd75d78
Oleg Naumov <naumov.olega@gmail.com>
2014-04-17 01:01:15 +0400

    Интерфейс для методов работы с БД, для STUB_LINK

---
+++ src/main/java/dao/interfaces/StubLinkDAO.java
@@ -0,0 +1,16 @@
+package dao.interfaces;
+
+import entity.StubLink;
+
+import java.util.List;
+
+/**
+ * User: Олег Наумов
+ * Date: 4/16/14 Time: 10:27 PM
+ */
+
+public interface StubLinkDAO {
+
+    List<StubLink> getStubLinksByCuId(Long cuId);
+
+}
