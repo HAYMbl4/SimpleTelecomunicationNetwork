44ce619ab06e41eb5b1b81fc3653f43918c06f58
Oleg Naumov <naumov.olega@gmail.com>
2014-04-18 09:58:40 +0400

    Реализация метода контейнера для подключений

--- src/main/java/entity/CableLinks.java
+++ src/main/java/entity/CableLinks.java
@@ -1,9 +1,61 @@
 package entity;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * User: Олег Наумов
  * Date: 4/16/14 Time: 9:29 AM
  */
 
-public class CableLinks {
+@Entity
+@Table(name = "cable_links")
+public class CableLinks implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "GEN_CABLE_LINKS")
+    @SequenceGenerator(name = "GEN_CABLE_LINKS", sequenceName = "GEN_CABLE_LINKS", allocationSize = 1)
+    @Column(name = "cable_links_id")
+    private Long cableLinksId;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY, targetEntity = StubLink.class)
+    @JoinColumn(name = "stub_link_id")
+    private StubLink stubLink;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY, targetEntity = StubLink.class)
+    @JoinColumn(name = "liked_stub_link_id")
+    private StubLink linkedStubLink;
+
+    public CableLinks() {
+    }
+
+    public CableLinks(StubLink stubLink, StubLink linkedStubLink) {
+        this.stubLink = stubLink;
+        this.linkedStubLink = linkedStubLink;
+    }
+
+    public Long getCableLinksId() {
+        return cableLinksId;
+    }
+
+    public void setCableLinksId(Long cableLinksId) {
+        this.cableLinksId = cableLinksId;
+    }
+
+    public StubLink getStubLink() {
+        return stubLink;
+    }
+
+    public void setStubLink(StubLink stubLink) {
+        this.stubLink = stubLink;
+    }
+
+    public StubLink getLinkedStubLink() {
+        return linkedStubLink;
+    }
+
+    public void setLinkedStubLink(StubLink linkedStubLink) {
+        this.linkedStubLink = linkedStubLink;
+    }
+
 }
