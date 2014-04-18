f85d15b97039ddc78c5f1962808c091b0dd0609e
Oleg Naumov <naumov.olega@gmail.com>
2014-04-19 02:45:20 +0400

    Добавил в интерфес метод получения ID подключаемых стабов

--- src/main/java/dao/interfaces/CableLinksDAO.java
+++ src/main/java/dao/interfaces/CableLinksDAO.java
@@ -1,9 +1,16 @@
 package dao.interfaces;
 
+import entity.StubLink;
+
+import java.util.List;
+
 /**
  * User: Олег Наумов
  * Date: 4/16/14 Time: 9:28 AM
  */
 
 public interface CableLinksDAO {
+
+    List<Long> getLinkedStubLinkByStubLinkId(Long stubLinkId);
+
 }
