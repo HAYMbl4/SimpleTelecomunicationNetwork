0f4e6789786f0fec99dccf87e1ec1b1747ab3d09
Oleg Naumov <naumov.olega@gmail.com>
2014-04-19 02:38:23 +0400

    Класс контейнр для сбора данных по точкам и передачи для отображения

---
+++ src/main/java/entity/ConnectionPointTable.java
@@ -0,0 +1,39 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * User: Олег Наумов
+ * Date: 4/18/14 Time: 11:54 PM
+ */
+
+public class ConnectionPointTable {
+
+    public ConnectionPoint connectionPoint;
+    public List<String> linkedCP;
+
+    public ConnectionPointTable() {
+    }
+
+    public ConnectionPointTable(ConnectionPoint connectionPoint, List<String> linkedCP) {
+        this.connectionPoint = connectionPoint;
+        this.linkedCP = linkedCP;
+    }
+
+    public ConnectionPoint getConnectionPoint() {
+        return connectionPoint;
+    }
+
+    public void setConnectionPoint(ConnectionPoint connectionPoint) {
+        this.connectionPoint = connectionPoint;
+    }
+
+    public List<String> getLinkedCP() {
+        return linkedCP;
+    }
+
+    public void setLinkedCP(List<String> linkedCP) {
+        this.linkedCP = linkedCP;
+    }
+
+}
