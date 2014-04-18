236b2cca23c2ab29afd0c08c461d67a374134a15
Oleg Naumov <naumov.olega@gmail.com>
2014-04-19 02:46:43 +0400

    Бин, в котором формируются данные для таблицы точек ОКУ

--- src/main/java/bean/ConnectionPointBean.java
+++ src/main/java/bean/ConnectionPointBean.java
@@ -1,9 +1,17 @@
 package bean;
 
+import dao.service.ServiceCableLinks;
 import dao.service.ServiceConnectionPoint;
+import dao.service.ServiceStubLink;
 import entity.ConnectionPoint;
+import entity.ConnectionPointTable;
+import entity.StubLink;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,11 +20,57 @@ import java.util.List;
  */
 
 @ManagedBean(name = "cpBean")
+//@RequestScoped
 public class ConnectionPointBean {
 
-    public List<ConnectionPoint> getListConnectionPoint() {
+    @ManagedProperty("#{param.cuId}")
+    private Long cuId;
+
+    public List<ConnectionPointTable> getListConnectionPointTable() {
         ServiceConnectionPoint serviceConnectionPoint = new ServiceConnectionPoint();
-        return serviceConnectionPoint.getListConnectionPoint();
+
+        List<ConnectionPointTable> cpTableList = new ArrayList<ConnectionPointTable>();
+
+        // получаем список точек по коннектору
+        List<ConnectionPoint> cpList = serviceConnectionPoint.getListConnectionPointByCuId(/*cuId*/2L);
+
+        ServiceStubLink serviceStubLink = new ServiceStubLink();
+        ServiceCableLinks serviceCableLinks = new ServiceCableLinks();
+        // проходим по списку точек и ищем подключения к ней
+        for (ConnectionPoint cpL: cpList) {
+            System.out.println("точка = " + cpL.getCpName());
+            // получаем список стабов, к которым подключена точка
+            if (serviceStubLink.getStubLinkIdByCpId(cpL.getCpId()) != null) {
+                List<Long> linkedStubLinkList = serviceCableLinks.getLinkedStubLinkByStubLinkId(serviceStubLink.getStubLinkIdByCpId(cpL.getCpId()));
+                for (Long l: linkedStubLinkList) { // формируем название подключаемых точек
+                    List<String> nameLinkedCp = new ArrayList<String>();
+                    StubLink stubLink = serviceStubLink.getStubLinkByStubLinkId(l.longValue());
+                    nameLinkedCp.add(stubLink.getNode().getNodeType().getNodeTypeShortName()+ "" // тип узла
+                                   + stubLink.getNode().getNodeName() + "-" // номер узла
+                                   + stubLink.getConnectionUnit().getCuNumber() + "-" // номер коннектора
+                                   + stubLink.getConnectionPoint().getCpName() // номер точки
+                    );
+                for (String s: nameLinkedCp) {
+                    System.out.println("Подключенные точки: " + s.toString());
+                }
+                // передаем собранные данные в контейнер для отображения
+                cpTableList.add(new ConnectionPointTable(cpL, nameLinkedCp));
+                }
+            } else {
+                cpTableList.add(new ConnectionPointTable(cpL,null));
+            }
+
+        }
+
+        return null;
+    }
+
+    public Long getCuId() {
+        return cuId;
+    }
+
+    public void setCuId(Long cuId) {
+        this.cuId = cuId;
     }
 
 }
