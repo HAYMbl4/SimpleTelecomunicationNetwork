abcfacf434f20612dde01fc070fcd350496995bb
Oleg Naumov <naumov.olega@gmail.com>
2014-04-20 03:08:05 +0400

    Правка + реализация toString()

--- src/main/java/entity/mapping/CableLink.java
+++ src/main/java/entity/mapping/CableLink.java
@@ -1,4 +1,4 @@
-package entity;
+package entity.mapping;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -10,12 +10,12 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "cable_link")
-public class CableLinks implements Serializable {
+public class CableLink implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "GEN_CABLE_LINKS")
     @SequenceGenerator(name = "GEN_CABLE_LINKS", sequenceName = "GEN_CABLE_LINKS", allocationSize = 1)
-    @Column(name = "cable_links_id")
+    @Column(name = "cable_link_id")
     private Long cableLinksId;
 
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY, targetEntity = StubLink.class)
@@ -26,10 +26,10 @@ public class CableLinks implements Serializable {
     @JoinColumn(name = "linked_stub_link_id")
     private StubLink linkedStubLink;
 
-    public CableLinks() {
+    public CableLink() {
     }
 
-    public CableLinks(StubLink stubLink, StubLink linkedStubLink) {
+    public CableLink(StubLink stubLink, StubLink linkedStubLink) {
         this.stubLink = stubLink;
         this.linkedStubLink = linkedStubLink;
     }
@@ -58,4 +58,10 @@ public class CableLinks implements Serializable {
         this.linkedStubLink = linkedStubLink;
     }
 
+    @Override
+    public String toString() {
+        return "CableLink = { nodeId = " + stubLink.getNode().getNodeId() + ", cuId = " + stubLink.getConnectionUnit().getCuId() +
+                ", cpId = " + stubLink.getConnectionPoint().getCpId() + " - linkedNodeId = " + linkedStubLink.getNode().getNodeId() +
+                ", linkedCuId = " + linkedStubLink.getConnectionUnit().getCuId() + ", linkedCpId = " + linkedStubLink.getConnectionPoint().getCpId() + "}";
+    }
 }
