722e0a415765c6170842add1f2592fd16ec8beb8
Oleg Naumov <naumov.olega@gmail.com>
2014-04-22 22:32:49 +0400

    Реализация метода проверки, занята точка или нет

--- src/main/java/dao/service/ServiceConnectionPoint.java
+++ src/main/java/dao/service/ServiceConnectionPoint.java
@@ -2,12 +2,14 @@ package dao.service;
 
 import dao.interfaces.ConnectionPointDAO;
 import entity.mapping.ConnectionPoint;
+import entity.mapping.StubLink;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  *
@@ -34,6 +36,22 @@ public class ServiceConnectionPoint implements ConnectionPointDAO {
         session.close();
 
         return cpList;
+    }
+
+    public String usedCp(Long cpId) {
+
+        ServiceConnectionPoint serviceConnectionPoint = new ServiceConnectionPoint();
+
+        Session session = serviceConnectionPoint.getSessionFactory().openSession();
+
+        Query query = session.createQuery("from StubLink where connectionPoint.cpId = :cpId");
+        query.setParameter("cpId", cpId);
+        try {
+        StubLink stubLink = (StubLink) query.iterate().next();
+            return "Подключена";
+        } catch (NoSuchElementException ex) {
+            return "";
+        }
     }
 
     protected SessionFactory getSessionFactory() {
