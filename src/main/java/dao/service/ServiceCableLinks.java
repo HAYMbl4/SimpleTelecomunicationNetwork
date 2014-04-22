25ff61fefec5a52158990600f81f06d974f63de3
Oleg Naumov <naumov.olega@gmail.com>
2014-04-22 22:32:20 +0400

    Реализация методов получения подключений по ID узла и ОКУ

--- src/main/java/dao/service/ServiceCableLinks.java
+++ src/main/java/dao/service/ServiceCableLinks.java
@@ -23,35 +23,6 @@ public class ServiceCableLinks implements CableLinksDAO {
 
     private static final Logger logger = LoggerFactory.getLogger("progTrace");
 
-    // пока не используется, может понадобится
-
-    public List<Long> getLinkedStubLinkByStubLinkId(Long stubLinkId) {
-//
-//        ServiceCableLinks serviceCableLinks = new ServiceCableLinks();
-//        Session session = serviceCableLinks.getSessionFactory().openSession();
-//
-//        List<Long> linkedStubLinkList = new ArrayList<Long>();
-//        Query query = session.createQuery("select linkedStubLink.stubLinkId from CableLink where stubLink.stubLinkId = :stubLinkId");
-//        query.setParameter("stubLinkId",stubLinkId);
-//        if (query.list().size() != 0) {
-//           linkedStubLinkList.addAll(query.list());
-//        }
-//
-//        query = session.createQuery("select stubLink.stubLinkId from CableLink where linkedStubLink.stubLinkId = :stubLinkId");
-//        query.setParameter("stubLinkId",stubLinkId);
-//        if (query.list().size() != 0) {
-//            linkedStubLinkList.addAll(query.list());
-//        }
-//
-//        for (Long sl: linkedStubLinkList) {
-////            System.out.println(sl.toString());
-//        }
-//
-//        session.close();
-//
-        return null;
-    }
-
     public List<CableLinkTable> getCableLinksByNodeId(Long nodeId) {
 
         ServiceCableLinks serviceCableLinks = new ServiceCableLinks();
@@ -111,8 +82,76 @@ public class ServiceCableLinks implements CableLinksDAO {
             }
         }
 
-        // без этого SOUT начинает кидать ошибку "Exception in thread "main" org.hibernate.LazyInitializationException: could not initialize proxy - no Session"
-        // todo: почему???
+        for (CableLinkTable clT: cableLinkTableList) {
+            System.out.println("От " + clT.getStubLink().toString() + " К " + clT.getLinkedStubLink().toString());
+        }
+
+        logger.info("----------------------------------------");
+        logger.trace("Закрываем сессию");
+        session.close();
+
+        return cableLinkTableList;
+    }
+
+    public List<CableLinkTable> getCableLinksByCuId(Long cuId) {
+
+        ServiceCableLinks serviceCableLinks = new ServiceCableLinks();
+
+        // получаем список стабов, по которым будем собирать подключения
+        ServiceStubLink serviceStubLink = new ServiceStubLink();
+        List<StubLink> stubLinkList = serviceStubLink.getStubLinksByCuId(cuId);
+
+        List<CableLink> cableLinkList;
+
+        // для сбора информации по подключениям
+        List<CableLinkTable> cableLinkTableList = new ArrayList<CableLinkTable>();
+        StubLink stubLink;
+        StubLink linkedStubLink;
+        //
+
+        logger.info("----------------------------------------");
+        logger.trace("Открываем сессию");
+        Session session = serviceCableLinks.getSessionFactory().openSession();
+
+        logger.info("----------------------------------------");
+        logger.trace("Выбираем подключения по полученным стабам");
+        for (StubLink sl: stubLinkList) {
+            Query query;
+            logger.info("----------------------------------------");
+            logger.trace("Ищем случаи, когда стаб с ID = " + sl.getStubLinkId() + " с левой стороны");
+            query = session.createQuery("from CableLink where stubLink.stubLinkId = :stubLinkId");
+            query.setParameter("stubLinkId", sl.getStubLinkId());
+            if (query.list().size() != 0) {
+                cableLinkList = query.list();
+                // передаем данные в контейнер для отображения
+                logger.info("----------------------------------------");
+                logger.trace("Передаем в контейнер по подключениям следующие подключения:");
+                for (CableLink cl: cableLinkList) {
+                    stubLink = cl.getStubLink();
+                    linkedStubLink = cl.getLinkedStubLink();
+                    logger.trace(stubLink.toString() + " - " + linkedStubLink.toString());
+                    cableLinkTableList.add(new CableLinkTable(stubLink, linkedStubLink));
+                }
+            }
+
+            logger.info("----------------------------------------");
+            logger.trace("Ищем случаи, когда стаб с ID = " + sl.getStubLinkId() + " с правой стороны");
+            query = session.createQuery("from CableLink where linkedStubLink.stubLinkId = :stubLinkId");
+            query.setParameter("stubLinkId", sl.getStubLinkId());
+            logger.info("----------------------------------------");
+            logger.trace("Передаем в контейнер по подключениям следующие подключения:");
+            if (query.list().size() != 0) {
+                cableLinkList = query.list();
+                // передаем данные в контейнер для отображения, меняя направление
+                for (CableLink cl: cableLinkList) {
+                    stubLink = cl.getLinkedStubLink();
+                    linkedStubLink = cl.getStubLink();
+                    logger.trace(stubLink.toString() + " - " + linkedStubLink.toString());
+                    cableLinkTableList.add(new CableLinkTable(stubLink,linkedStubLink));
+                }
+            }
+        }
+
         for (CableLinkTable clT: cableLinkTableList) {
             System.out.println("От " + clT.getStubLink().toString() + " К " + clT.getLinkedStubLink().toString());
         }
