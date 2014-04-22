bd03eb1d66294aada71a7fa2cf916aa10c958485
Oleg Naumov <naumov.olega@gmail.com>
2014-04-22 22:42:36 +0400

    Добавил возможность выбора, типа узла, для выборки

--- src/main/java/bean/NodeBean.java
+++ src/main/java/bean/NodeBean.java
@@ -1,11 +1,13 @@
 package bean;
 
 import dao.service.ServiceNode;
+import dao.service.ServiceNodeType;
 import entity.mapping.Node;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,9 +21,34 @@ import java.util.List;
 @SessionScoped
 public class NodeBean implements Serializable {
 
+    public String nodeTypeValue = "All";
+
+    public List<String> getListNodeType() {
+        ServiceNodeType serviceNodeType = new ServiceNodeType();
+        List<String> nodeTypeList = new ArrayList<String>();
+        nodeTypeList = serviceNodeType.getListNodeType();
+        nodeTypeList.add("All");
+        return nodeTypeList;
+    }
+
     public List<Node> getListNode() {
         ServiceNode serviceNode = new ServiceNode();
-        return serviceNode.getListNode();
+        System.out.println("----------------------------------------------------------- " + nodeTypeValue);
+        if (!nodeTypeValue.equals("All")) {
+            System.out.println("----------------------------------- hi from not ALL " + nodeTypeValue);
+            return serviceNode.getNodeByNodeTypeName(nodeTypeValue);
+        } else {
+            System.out.println("------------------------------- hi from ALL " + nodeTypeValue);
+            return serviceNode.getListNode();
+        }
+    }
+
+    public String getNodeTypeValue() {
+        return nodeTypeValue;
+    }
+
+    public void setNodeTypeValue(String nodeTypeValue) {
+        this.nodeTypeValue = nodeTypeValue;
     }
 
 }
