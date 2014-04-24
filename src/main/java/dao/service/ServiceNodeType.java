7c227fd50f6cf6021cc09984b72106a2501226b5
Oleg Naumov <naumov.olega@gmail.com>
2014-04-24 23:14:06 +0400

    Реализация метода создания типа узла

--- src/main/java/dao/service/ServiceNodeType.java
+++ src/main/java/dao/service/ServiceNodeType.java
@@ -33,12 +33,12 @@ public class ServiceNodeType implements NodeTypeDAO {
         Session session = serviceNodeType.getSessionFactory().openSession();
 
         logger.info("----------------------------------------");
-        logger.trace("Получаем писок типов узлов: ");
+        logger.trace("Получаем cписок типов узлов: ");
         Query query = session.createQuery("select nodeTypeName from NodeType");
         List<String> nodeTypeList = query.list();
         for (String nt: nodeTypeList) {
             System.out.println(nt.toString());
-            System.out.println(nt);
+            logger.trace(nt.toString());
         }
 
         logger.info("----------------------------------------");
@@ -46,6 +46,27 @@ public class ServiceNodeType implements NodeTypeDAO {
         session.close();
 
         return nodeTypeList;
+    }
+
+    public void createNodeType(NodeType nodeType) {
+
+        ServiceNodeType serviceNodeType = new ServiceNodeType();
+
+        logger.info("----------------------------------------");
+        logger.trace("Открываем сессию");
+        Session session = serviceNodeType.getSessionFactory().openSession();
+        session.beginTransaction();
+        logger.info("----------------------------------------");
+        logger.trace("Создаем запись в таблице NODE_TYPE со следующими параметрами: ");
+        logger.info("NODE_TYPE_NAME = " + nodeType.getNodeTypeName());
+        logger.info("NODE_TYPE_SHORT_NAME = " + nodeType.getNodeTypeShortName());
+        session.save(nodeType);
+        session.getTransaction().commit();
+        logger.trace("Операция прошла успешно");
+        logger.info("----------------------------------------");
+        logger.trace("Закрываем сессию");
+        session.close();
+
     }
 
     protected SessionFactory getSessionFactory() {
