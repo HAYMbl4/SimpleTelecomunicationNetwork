d36d33e454ab5ac6db2e1bde859938d5c633aeb5
Oleg Naumov <naumov.olega@gmail.com>
2014-04-28 23:10:06 +0400

    Реализция создания узла

--- src/main/java/bean/NodeBean.java
+++ src/main/java/bean/NodeBean.java
@@ -3,6 +3,8 @@ package bean;
 import dao.service.ServiceNode;
 import dao.service.ServiceNodeType;
 import entity.mapping.Node;
+import entity.mapping.NodeType;
+import org.hibernate.exception.ConstraintViolationException;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
@@ -22,6 +24,13 @@ import java.util.List;
 public class NodeBean implements Serializable {
 
     public String nodeTypeValue = "All";
+    public String nodeName;
+    public String region;
+    public String street;
+    public String house;
+    public String nodeNote;
+    public String resoultMess;
+    public String styleMess = "hideMess";
 
     public List<String> getListNodeType() {
         ServiceNodeType serviceNodeType = new ServiceNodeType();
@@ -29,6 +38,11 @@ public class NodeBean implements Serializable {
         nodeTypeList = serviceNodeType.getListNodeType();
         nodeTypeList.add("All");
         return nodeTypeList;
+    }
+
+    public List<String> getListNodeTypeForCreate() {
+        ServiceNodeType serviceNodeType = new ServiceNodeType();
+        return serviceNodeType.getListNodeType();
     }
 
     public List<Node> getListNode() {
@@ -40,6 +54,37 @@ public class NodeBean implements Serializable {
         }
     }
 
+    public void createNode() {
+
+        ServiceNode serviceNode = new ServiceNode();
+        ServiceNodeType serviceNodeType = new ServiceNodeType();
+
+        NodeType nodeType = serviceNodeType.getNodeTypeByName(nodeTypeValue);
+        try {
+            serviceNode.createNode(new Node (nodeName, nodeType, region, street, house, nodeNote));
+            resoultMess = "Узел успешно создан";
+            styleMess = "createNode";
+        } catch (ConstraintViolationException ex) {
+            resoultMess = "Узел с такими параметрами уже есть!";
+            styleMess = "createNodeError";
+        } catch (Exception ex) {
+            resoultMess = "ошибка";
+            styleMess = "createNodeError";
+        }
+
+        System.out.println(resoultMess);
+    }
+
+    public void cleanParam() {
+        nodeName = "";
+        region = "";
+        street = "";
+        house = "";
+        nodeNote = "";
+        resoultMess = "";
+        styleMess = "hideMess";
+    }
+
     public String getNodeTypeValue() {
         return nodeTypeValue;
     }
@@ -48,4 +93,59 @@ public class NodeBean implements Serializable {
         this.nodeTypeValue = nodeTypeValue;
     }
 
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getNodeNote() {
+        return nodeNote;
+    }
+
+    public void setNodeNote(String nodeNote) {
+        this.nodeNote = nodeNote;
+    }
+
+    public String getResoultMess() {
+        return resoultMess;
+    }
+
+    public void setResoultMess(String resoultMess) {
+        this.resoultMess = resoultMess;
+    }
+
+    public String getStyleMess() {
+        return styleMess;
+    }
+
+    public void setStyleMess(String styleMess) {
+        this.styleMess = styleMess;
+    }
 }
